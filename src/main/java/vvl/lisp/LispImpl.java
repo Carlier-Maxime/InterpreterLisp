74207784ae3d29740a58e8506bfa38bf2195bd8d
M_EtOuais <28m06c02@gmail.com>
2024-01-16 23:04:10 +0100

    add empty lisp implementation

---
+++ src/main/java/vvl/lisp/LispImpl.java
@@ -0,0 +1,13 @@
+package vvl.lisp;
+
+public class LispImpl implements Lisp {
+    @Override
+    public LispItem parse(String expr) throws LispError {
+        return null;
+    }
+
+    @Override
+    public LispItem evaluate(LispItem ex) throws LispError {
+        return null;
+    }
+}
