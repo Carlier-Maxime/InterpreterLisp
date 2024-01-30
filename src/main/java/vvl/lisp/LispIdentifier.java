7351837f68681a34a3afd1567e7b903a2867e81a
M_EtOuais <28m06c02@gmail.com>
2024-01-30 23:36:03 +0100

    fix name for static final var (respect naming convention)

--- src/main/java/vvl/lisp/LispIdentifier.java
+++ src/main/java/vvl/lisp/LispIdentifier.java
@@ -13,24 +13,24 @@ import java.util.regex.Pattern;
  *
  */
 public class LispIdentifier implements LispItem {
-	private static final String identifierRegex = "^[!$%&*/:<=>?^\\-+_~a-zA-Z][!$%&*/:<=>?^\\-+_~a-zA-Z0-9]*$";
-	private static final Pattern identifierPattern = Pattern.compile(identifierRegex);
-	private static final Map<String, LispFunction> functions = new HashMap<>() {};
+	private static final String IDENTIFIER_REGEX = "^[!$%&*/:<=>?^\\-+_~a-zA-Z][!$%&*/:<=>?^\\-+_~a-zA-Z0-9]*$";
+	private static final Pattern IDENTIFIER_PATTERN = Pattern.compile(IDENTIFIER_REGEX);
+	private static final Map<String, LispFunction> FUNCTIONS = new HashMap<>() {};
 	static {
-		functions.put("not", LispFunction.NOT);
-		functions.put("and", LispFunction.AND);
-		functions.put("or", LispFunction.OR);
-		functions.put(">", LispFunction.GREATER);
-		functions.put(">=", LispFunction.GREATER_OR_EQUALS);
-		functions.put("<", LispFunction.LESSER);
-		functions.put("<=", LispFunction.LESSER_OR_EQUALS);
-		functions.put("=", LispFunction.IS_EQUALS);
+		FUNCTIONS.put("not", LispFunction.NOT);
+		FUNCTIONS.put("and", LispFunction.AND);
+		FUNCTIONS.put("or", LispFunction.OR);
+		FUNCTIONS.put(">", LispFunction.GREATER);
+		FUNCTIONS.put(">=", LispFunction.GREATER_OR_EQUALS);
+		FUNCTIONS.put("<", LispFunction.LESSER);
+		FUNCTIONS.put("<=", LispFunction.LESSER_OR_EQUALS);
+		FUNCTIONS.put("=", LispFunction.IS_EQUALS);
 	}
 
 	private final String id;
 	
 	public LispIdentifier(String id) throws LispError {
-		if (!identifierPattern.matcher(id).matches()) throw new LispError("This identifier '"+id+"' not respect regex : "+identifierRegex);
+		if (!IDENTIFIER_PATTERN.matcher(id).matches()) throw new LispError("This identifier '"+id+"' not respect regex : "+ IDENTIFIER_REGEX);
 		this.id = id;
 	}
 	
@@ -53,7 +53,7 @@ public class LispIdentifier implements LispItem {
 
 	@Override
 	public LispItem eval(ConsList<LispItem> items) throws LispError {
-		LispItem func = functions.get(id);
+		LispItem func = FUNCTIONS.get(id);
 		if (func==null) throw new LispError("Identifier '"+id+"' not implemented");
 		return func.eval(items);
 	}
