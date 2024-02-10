98bb2905cd7dc8233537a94be94880cce314fa44
M_EtOuais <28m06c02@gmail.com>
2024-02-10 12:47:22 +0100

    add vars

--- src/main/java/vvl/lisp/LispIdentifier.java
+++ src/main/java/vvl/lisp/LispIdentifier.java
@@ -18,6 +18,7 @@ public class LispIdentifier implements LispItem {
 	private static final String IDENTIFIER_REGEX = "^[!$%&*/:<=>?^\\-+_~a-zA-Z][!$%&*/:<=>?^\\-+_~a-zA-Z0-9]*$";
 	private static final Pattern IDENTIFIER_PATTERN = Pattern.compile(IDENTIFIER_REGEX);
 	private static final Map<String, LispItem> BUILTIN = new HashMap<>() {};
+	private static final Map<String, LispItem> VARS = new HashMap<>() {};
 	static {
 		BUILTIN.put("not", LogicalOperations.NOT);
 		BUILTIN.put("and", LogicalOperations.AND);
@@ -73,5 +74,29 @@ public class LispIdentifier implements LispItem {
 
 	public String getId() {
 		return id;
+	}
+
+	public boolean isBuiltin() {
+		return BUILTIN.get(id)!=null;
+	}
+
+	public boolean isVars() {
+		return VARS.get(id)!=null;
+	}
+
+	public static LispError notValidIdentifier(String id) {
+		return new LispError(id+" is not a valid identifier");
+	}
+
+	public LispItem setVar(LispItem item, boolean replace) throws LispError {
+		if (isBuiltin()) throw notValidIdentifier(id);
+		if (isVars()) {
+			if (!replace) throw notValidIdentifier(id);
+			VARS.replace(id, item);
+		} else {
+			if (replace) throw new LispError(id+" is undefined");
+			VARS.put(id, item);
+		}
+		return item;
 	}
 }
