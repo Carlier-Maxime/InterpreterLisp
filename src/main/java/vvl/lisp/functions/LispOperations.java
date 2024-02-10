52d03059f1711d4f3876fbffbc54becc9ebf09d9
M_EtOuais <28m06c02@gmail.com>
2024-02-10 22:10:07 +0100

    rm redundant check type

--- src/main/java/vvl/lisp/functions/LispOperations.java
+++ src/main/java/vvl/lisp/functions/LispOperations.java
@@ -28,11 +28,7 @@ public class LispOperations {
         }
     }, LispPair.class);
     public static final LispFunction CDR = new LispFunction(params -> ((LispPair) params.car()).cdr(), LispPair.class);
-    public static final LispFunction SET_VAR = new LispFunction(params -> {
-        var identifier = params.cdr().carNoEval();
-        if (identifier instanceof LispIdentifier) return params.getContext().setVar(((LispIdentifier) identifier).getId(), params.cdr().cdr().car(), ((LispBoolean) params.car()).value());
-        throw LispContext.notValidIdentifier(identifier.toString());
-    }, LispBoolean.class, LispIdentifier.class, LispItem.class);
+    public static final LispFunction SET_VAR = new LispFunction(params -> params.getContext().setVar(((LispIdentifier) params.cdr().carNoEval()).getId(), params.cdr().cdr().car(), ((LispBoolean) params.car()).value()), LispBoolean.class, LispIdentifier.class, LispItem.class);
     public static final LispFunction DEFINE = new LispFunction(params -> SET_VAR.apply(params.prepend(LispBoolean.FALSE)), LispIdentifier.class, LispItem.class);
     public static final LispFunction SET = new LispFunction(params -> SET_VAR.apply(params.prepend(LispBoolean.TRUE)), LispIdentifier.class, LispItem.class);
     public static final LispFunction LAMBDA = new LispFunction(params -> new LispLambda(((LispExpression) params.carNoEval()), params.cdr().carNoEval()), LispExpression.class, LispItem.class);
