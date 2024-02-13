68ad8c899e52145489428e5ed4d98601626a575d
M_EtOuais <28m06c02@gmail.com>
2024-02-13 22:19:36 +0100

    add makeComparison for simplify code

--- src/main/java/vvl/lisp/functions/ComparisonOperations.java
+++ src/main/java/vvl/lisp/functions/ComparisonOperations.java
@@ -4,25 +4,22 @@ import vvl.lisp.LispBoolean;
 import vvl.lisp.LispNumber;
 import vvl.util.ConsList;
 
+import java.util.function.BiFunction;
+
 public class ComparisonOperations {
     private ComparisonOperations() {}
-    public static final LispFunction CHECK_CONDITION_FOR_PAIRS = new LispFunction(params -> {
-        int size = params.size() - 2;
-        LispFunction cdn = (LispFunction) params.car();
-        params = params.cdr();
-        for (var i = 0; i < size; i++)
-            if (cdn.apply(params.getContext(), ConsList.asList(params.car(), (params = params.cdr()).car())) == LispBoolean.FALSE)
-                return LispBoolean.FALSE;
-        return LispBoolean.TRUE;
-    }, true, LispFunction.class, LispNumber.class, LispNumber.class);
-    public static final LispFunction GREATER_CONDITION = new LispFunction(params -> LispBoolean.valueOf(((LispNumber) params.car()).compareTo((LispNumber) params.cdr().car()) > 0), LispNumber.class, LispNumber.class);
-    public static final LispFunction GREATER = new LispFunction(params -> CHECK_CONDITION_FOR_PAIRS.apply(params.prepend(GREATER_CONDITION.quote())), true, LispNumber.class);
-    public static final LispFunction LESSER_CONDITION = new LispFunction(params -> LispBoolean.valueOf(((LispNumber) params.car()).compareTo((LispNumber) params.cdr().car()) < 0), LispNumber.class, LispNumber.class);
-    public static final LispFunction LESSER = new LispFunction(params -> CHECK_CONDITION_FOR_PAIRS.apply(params.prepend(LESSER_CONDITION.quote())), true, LispNumber.class);
-    public static final LispFunction EQUALS_CONDITION = new LispFunction(params -> LispBoolean.valueOf(((LispNumber) params.car()).compareTo((LispNumber) params.cdr().car()) == 0), LispNumber.class, LispNumber.class);
-    public static final LispFunction IS_EQUALS = new LispFunction(params -> CHECK_CONDITION_FOR_PAIRS.apply(params.prepend(EQUALS_CONDITION.quote())), true, LispNumber.class);
-    public static final LispFunction GREATER_OR_EQUALS_CONDITION = new LispFunction(params -> LispBoolean.valueOf(((LispNumber) params.car()).compareTo((LispNumber) params.cdr().car()) >= 0), LispNumber.class, LispNumber.class);
-    public static final LispFunction GREATER_OR_EQUALS = new LispFunction(params -> CHECK_CONDITION_FOR_PAIRS.apply(params.prepend(GREATER_OR_EQUALS_CONDITION.quote())), true, LispNumber.class);
-    public static final LispFunction LESSER_OR_EQUALS_CONDITION = new LispFunction(params -> LispBoolean.valueOf(((LispNumber) params.car()).compareTo((LispNumber) params.cdr().car()) <= 0), LispNumber.class, LispNumber.class);
-    public static final LispFunction LESSER_OR_EQUALS = new LispFunction(params -> CHECK_CONDITION_FOR_PAIRS.apply(params.prepend(LESSER_OR_EQUALS_CONDITION.quote())), true, LispNumber.class);
+
+    private static LispFunction makeComparison(BiFunction<LispNumber, LispNumber, Boolean> cdn) {
+        return new LispFunction(params -> {
+            var size = params.size() - 1;
+            for (var i = 0; i < size; i++) if (!cdn.apply((LispNumber) params.car(), (LispNumber) (params = params.cdr()).car())) return LispBoolean.FALSE;
+            return LispBoolean.TRUE;
+        }, true, LispNumber.class, LispNumber.class);
+    }
+
+    public static final LispFunction GREATER = makeComparison((a, b) -> a.compareTo(b) > 0);
+    public static final LispFunction LESSER = makeComparison((a, b) -> a.compareTo(b) < 0);
+    public static final LispFunction IS_EQUALS = makeComparison((a, b) -> a.compareTo(b) == 0);
+    public static final LispFunction GREATER_OR_EQUALS = makeComparison((a, b) -> a.compareTo(b) >= 0);
+    public static final LispFunction LESSER_OR_EQUALS = makeComparison((a, b) -> a.compareTo(b) <= 0);
 }
