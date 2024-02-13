dd2ac3d3437cf91865b7f76642fa34ba9556d36d
M_EtOuais <28m06c02@gmail.com>
2024-02-13 21:23:18 +0100

    add makeVariadicFunction for simplify code

--- src/main/java/vvl/lisp/functions/MathOperations.java
+++ src/main/java/vvl/lisp/functions/MathOperations.java
@@ -1,51 +1,34 @@
 package vvl.lisp.functions;
 
+import org.jetbrains.annotations.NotNull;
 import vvl.lisp.LispNumber;
 
 import java.math.BigInteger;
+import java.util.function.BinaryOperator;
 
 public class MathOperations {
     private MathOperations() {}
-    public static final LispFunction ADD = new LispFunction(params -> {
-        var result = new LispNumber(BigInteger.valueOf(0));
-        var size = params.size();
-        for (var i = 0; i < size; i++) {
-            result = result.add((LispNumber) params.car());
-            params = params.cdr();
-        }
-        return result;
-    }, true, LispNumber.class);
-    public static final LispFunction MUL = new LispFunction(params -> {
-        var result = new LispNumber(BigInteger.valueOf(1));
-        var size = params.size();
-        for (var i = 0; i < size; i++) {
-            result = result.mul((LispNumber) params.car());
-            params = params.cdr();
-        }
-        return result;
-    }, true, LispNumber.class);
+
+    private static LispFunction makeVariadicFunction(@NotNull BinaryOperator<LispNumber> op, @NotNull LispNumber initValue) {
+        return new LispFunction(params -> {
+            var result = initValue;
+            var size = params.size();
+            for (var i = 0; i < size; i++) {
+                result = op.apply(result, (LispNumber) params.car());
+                params = params.cdr();
+            }
+            return result;
+        }, true, LispNumber.class);
+    }
+    public static final LispFunction ADD = makeVariadicFunction(LispNumber::add, new LispNumber(BigInteger.valueOf(0)));
+    public static final LispFunction MUL =  makeVariadicFunction(LispNumber::mul, new LispNumber(BigInteger.valueOf(1)));
     public static final LispFunction SUB = new LispFunction(params -> {
         if (params.size() > 2) throw LispFunction.INVALID_NUMBER_OF_OPERAND;
         var result = (LispNumber) params.car();
-        params = params.cdr();
-        var size = params.size();
-        for (var i = 0; i < size; i++) {
-            result = result.sub((LispNumber) params.car());
-            params = params.cdr();
-        }
-        return size == 0 ? result.sub(null) : result;
+        return result.sub(params.size()==1 ? null : (LispNumber) params.cdr().car());
     }, true, LispNumber.class, LispNumber.class);
-    public static final LispFunction DIV = new LispFunction(params -> {
-        var result = (LispNumber) params.car();
-        params = params.cdr();
-        var size = params.size();
-        for (var i = 0; i < size; i++) {
-            result = result.div((LispNumber) params.car());
-            params = params.cdr();
-        }
-        return result;
-    }, LispNumber.class, LispNumber.class);
 
+    public static final LispFunction DIV = new LispFunction(params -> ((LispNumber) params.car()).div((LispNumber) params.cdr().car()), LispNumber.class, LispNumber.class);
     public static final LispFunction MAX = new LispFunction(params -> ((LispNumber) params.car()).max((LispNumber) params.cdr().car()), LispNumber.class, LispNumber.class);
     public static final LispFunction MIN = new LispFunction(params -> ((LispNumber) params.car()).min((LispNumber) params.cdr().car()), LispNumber.class, LispNumber.class);
     public static final LispFunction POW = new LispFunction(params -> ((LispNumber) params.car()).pow((LispNumber) params.cdr().car()), LispNumber.class, LispNumber.class);
