e9cb33998b9149170ed70ae7c2bc4cdd3b4a9837
M_EtOuais <28m06c02@gmail.com>
2024-02-13 22:03:06 +0100

    reduce name of private method

--- src/main/java/vvl/lisp/functions/MathOperations.java
+++ src/main/java/vvl/lisp/functions/MathOperations.java
@@ -10,7 +10,7 @@ import java.util.function.UnaryOperator;
 public class MathOperations {
     private MathOperations() {}
 
-    private static LispFunction makeVariadicFunction(@NotNull BinaryOperator<LispNumber> op, @NotNull LispNumber initValue) {
+    private static LispFunction makeVariadic(@NotNull BinaryOperator<LispNumber> op, @NotNull LispNumber initValue) {
         return new LispFunction(params -> {
             var result = initValue;
             var size = params.size();
@@ -22,35 +22,35 @@ public class MathOperations {
         }, true, LispNumber.class);
     }
 
-    private static LispFunction makeBinaryOperationFunction(@NotNull BinaryOperator<LispNumber> op) {
+    private static LispFunction makeBinary(@NotNull BinaryOperator<LispNumber> op) {
         return new LispFunction(params -> op.apply(((LispNumber) params.car()), (LispNumber) params.cdr().car()), LispNumber.class, LispNumber.class);
     }
 
-    private static LispFunction makeUnaryOperationFunction(@NotNull UnaryOperator<LispNumber> op) {
+    private static LispFunction makeUnary(@NotNull UnaryOperator<LispNumber> op) {
         return new LispFunction(params -> op.apply((LispNumber) params.car()), LispNumber.class);
     }
-    public static final LispFunction ADD = makeVariadicFunction(LispNumber::add, new LispNumber(BigInteger.valueOf(0)));
-    public static final LispFunction MUL =  makeVariadicFunction(LispNumber::mul, new LispNumber(BigInteger.valueOf(1)));
+    public static final LispFunction ADD = makeVariadic(LispNumber::add, new LispNumber(BigInteger.valueOf(0)));
+    public static final LispFunction MUL =  makeVariadic(LispNumber::mul, new LispNumber(BigInteger.valueOf(1)));
     public static final LispFunction SUB = new LispFunction(params -> {
         if (params.size() > 2) throw LispFunction.INVALID_NUMBER_OF_OPERAND;
         var result = (LispNumber) params.car();
         return result.sub(params.size()==1 ? null : (LispNumber) params.cdr().car());
     }, true, LispNumber.class, LispNumber.class);
 
-    public static final LispFunction DIV = makeBinaryOperationFunction(LispNumber::div);
-    public static final LispFunction MAX = makeBinaryOperationFunction(LispNumber::max);
-    public static final LispFunction MIN = makeBinaryOperationFunction(LispNumber::min);
-    public static final LispFunction POW = makeBinaryOperationFunction(LispNumber::pow);
+    public static final LispFunction DIV = makeBinary(LispNumber::div);
+    public static final LispFunction MAX = makeBinary(LispNumber::max);
+    public static final LispFunction MIN = makeBinary(LispNumber::min);
+    public static final LispFunction POW = makeBinary(LispNumber::pow);
 
-    public static final LispFunction ABS = makeUnaryOperationFunction(LispNumber::abs);
-    public static final LispFunction CBRT = makeUnaryOperationFunction(LispNumber::cbrt);
-    public static final LispFunction CEIL = makeUnaryOperationFunction(LispNumber::ceil);
-    public static final LispFunction FLOOR = makeUnaryOperationFunction(LispNumber::floor);
-    public static final LispFunction LOG10 = makeUnaryOperationFunction(LispNumber::log10);
-    public static final LispFunction COS = makeUnaryOperationFunction(LispNumber::cos);
-    public static final LispFunction SIN = makeUnaryOperationFunction(LispNumber::sin);
-    public static final LispFunction RINT = makeUnaryOperationFunction(LispNumber::rint);
-    public static final LispFunction ROUND = makeUnaryOperationFunction(LispNumber::round);
-    public static final LispFunction SIGNUM = makeUnaryOperationFunction(LispNumber::signum);
-    public static final LispFunction SQRT = makeUnaryOperationFunction(LispNumber::sqrt);
+    public static final LispFunction ABS = makeUnary(LispNumber::abs);
+    public static final LispFunction CBRT = makeUnary(LispNumber::cbrt);
+    public static final LispFunction CEIL = makeUnary(LispNumber::ceil);
+    public static final LispFunction FLOOR = makeUnary(LispNumber::floor);
+    public static final LispFunction LOG10 = makeUnary(LispNumber::log10);
+    public static final LispFunction COS = makeUnary(LispNumber::cos);
+    public static final LispFunction SIN = makeUnary(LispNumber::sin);
+    public static final LispFunction RINT = makeUnary(LispNumber::rint);
+    public static final LispFunction ROUND = makeUnary(LispNumber::round);
+    public static final LispFunction SIGNUM = makeUnary(LispNumber::signum);
+    public static final LispFunction SQRT = makeUnary(LispNumber::sqrt);
 }
